d26fee0529969ff635b60e6d629445ab67daf6d2
Jared Hardinger <jaredhardinger@gmail.com>
2022-09-26 04:10:47 -0400

    create location poro and test

---
+++ spec/poros/location_poro_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Location PORO' do
+  it 'creates a PORO' do
+    coordinates = {
+      lat: 44.24,
+      lng: -82.32
+    }
+
+    poro = Location.new(coordinates)
+    expect(poro).to be_a(Location)
+    expect(poro.lat).to eq(coordinates[:lat])
+    expect(poro.lng).to eq(coordinates[:lng])
+  end
+end
