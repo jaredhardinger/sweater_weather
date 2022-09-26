8ec17bd62504ac7ee350d32ed918da283b45d3ba
Jared Hardinger <jaredhardinger@gmail.com>
2022-09-26 02:25:11 -0400

    Create/edit weather service and spec

---
+++ app/services/forecast_service.rb
@@ -0,0 +1,21 @@
+class ForecastService
+  def self.get_forecast(lat, lon)
+    response = conn.get('/data/2.5/onecall?') do |req|
+      req.params['lat'] = lat
+      req.params['lon'] = lon
+      req.params['exclude'] = 'minutely'
+      req.params['units'] = 'imperial'
+      req.params['appid'] = ENV['weather_api_key']
+    end
+    parse_json(response)
+  end
+
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+  def self.conn
+    Faraday.new(url: 'https://api.openweathermap.org')
+  end
+end
--- spec/services/forecast_service_spec.rb
+++ spec/services/forecast_service_spec.rb
@@ -3,14 +3,41 @@ require 'rails_helper'
 RSpec.describe "OpenWeather API Service" do
 	before do
 		json_response = File.read('spec/fixtures/manistee_weather.json')
-		stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall?lat=44.24&lon=-82.32&appid=#{ENV['weather_api_key']}").
+		stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall?lat=44.24&lon=-82.32&exclude=minutely&units=imperial&appid=#{ENV['weather_api_key']}").
 				to_return(status: 200, body: json_response)
 	end
 	
-	it 'gets a response' do
+	it 'gets a response with the correct attributes' do
 		lat = "44.24"
 		lon = "-82.32" 
-		response = ForecastService(lat, lon)
-		expect(response).to be_successful
+		response = ForecastService.get_forecast(lat, lon)
+		expect(response).to be_a Hash
+		expect(response).to have_key(:current)
+		current = response[:current]
+		expect(current).to have_key(:dt)
+		expect(current).to have_key(:sunrise)
+		expect(current).to have_key(:sunset)
+		expect(current).to have_key(:temp)
+		expect(current).to have_key(:feels_like)
+		expect(current).to have_key(:humidity)
+		expect(current).to have_key(:uvi)
+		expect(current).to have_key(:visibility)
+		current_weather = current[:weather][0]
+		expect(current_weather).to have_key(:description)
+		expect(current_weather).to have_key(:icon)
+		daily = response[:daily][0]
+		expect(daily).to have_key(:dt)
+		expect(daily).to have_key(:temp)
+		daily_weather = daily[:weather][0]
+		expect(daily_weather).to have_key(:description)
+		expect(daily_weather).to have_key(:icon)
+		hourly = response[:hourly][0]
+		expect(hourly).to have_key(:dt)
+		expect(hourly).to have_key(:temp)
+		hourly_weather = hourly[:weather][0]
+		expect(hourly_weather).to have_key(:description)
+		expect(hourly_weather).to have_key(:icon)
+
+		expect(response).to_not have_key(:minutely)
 	end 
 end
