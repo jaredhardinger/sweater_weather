d95475f9e58f900bbca6f01437871899e3d29851
Jared Hardinger <jaredhardinger@gmail.com>
2022-09-26 08:35:18 -0400

    Edit weather poro and test

---
+++ spec/facades/weather_facade_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Weather Facade' do 
+  before do
+		json_response = File.read('spec/fixtures/manistee_weather.json')
+		stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall?lat=44.24&lon=-82.32&exclude=minutely&units=imperial&appid=#{ENV['weather_api_key']}").
+				to_return(status: 200, body: json_response)
+	end
+  
+  it 'returns the weather when given lat lng' do
+		lat = "44.24"
+		lon = "-82.32" 
+    result = WeatherFacade.weather(lat, lng)
+
+    expect(result).to be_a(Weather)
+  end
+
+end
--- spec/poros/weather_poro_spec.rb
+++ spec/poros/weather_poro_spec.rb
@@ -5,45 +5,50 @@ RSpec.describe 'Weather PORO' do
     before do
       response = File.read('spec/fixtures/manistee_weather.json')
       weather = JSON.parse(response, symbolize_names: true)
-      @current_weather = weather[:current] 
-      @daily_weather = weather[:daily][0]
-      @hourly_weather = weather[:hourly][0]
+      @current = CurrentWeather.new(weather[:current])
+      @daily = DailyWeather.new(weather[:daily][0])
+      @hourly = HourlyWeather.new(weather[:hourly][0])
     end
 
     it 'creates a current weather PORO' do 
-      poro = CurrentWeather.new(@current_weather)
-      expect(poro).to be_a(CurrentWeather)
-      expect(poro.datetime).to be_a(String)
-      expect(poro.sunrise).to be_a(String)
-      expect(poro.sunset).to be_a(String)
-      expect(poro.temperature).to be_a(Float)
-      expect(poro.feels_like).to be_a(Float)
-      expect(poro.humidity).to be_a(Integer)
-      expect(poro.uvi).to be_a(Integer)
-      expect(poro.visibility).to be_a(Integer)
-      expect(poro.conditions).to be_a(String)
-      expect(poro.icon).to be_a(String)
+      expect(@current).to be_a(CurrentWeather)
+      expect(@current.datetime).to be_a(String)
+      expect(@current.sunrise).to be_a(String)
+      expect(@current.sunset).to be_a(String)
+      expect(@current.temperature).to be_a(Float)
+      expect(@current.feels_like).to be_a(Float)
+      expect(@current.humidity).to be_a(Integer)
+      expect(@current.uvi).to be_a(Integer)
+      expect(@current.visibility).to be_a(Integer)
+      expect(@current.conditions).to be_a(String)
+      expect(@current.icon).to be_a(String)
     end
 
     it 'creates a daily weather PORO' do
-      poro = DailyWeather.new(@daily_weather)
-      expect(poro).to be_a(DailyWeather)
-      expect(poro.date).to be_a(String)
-      expect(poro.sunrise).to be_a(String)
-      expect(poro.sunset).to be_a(String)
-      expect(poro.max_temp).to be_a(Float)
-      expect(poro.min_temp).to be_a(Float)
-      expect(poro.conditions).to be_a(String)
-      expect(poro.icon).to be_a(String)
+      expect(@daily).to be_a(DailyWeather)
+      expect(@daily.date).to be_a(String)
+      expect(@daily.sunrise).to be_a(String)
+      expect(@daily.sunset).to be_a(String)
+      expect(@daily.max_temp).to be_a(Float)
+      expect(@daily.min_temp).to be_a(Float)
+      expect(@daily.conditions).to be_a(String)
+      expect(@daily.icon).to be_a(String)
     end
 
     it 'creates an hourly weather PORO' do
-      poro = HourlyWeather.new(@hourly_weather)
-      expect(poro).to be_a(HourlyWeather)
-      expect(poro.time).to be_a(String)
-      expect(poro.temperature).to be_a(Float)
-      expect(poro.conditions).to be_a(String)
-      expect(poro.icon).to be_a(String)
+      expect(@hourly).to be_a(HourlyWeather)
+      expect(@hourly.time).to be_a(String)
+      expect(@hourly.temperature).to be_a(Float)
+      expect(@hourly.conditions).to be_a(String)
+      expect(@hourly.icon).to be_a(String)
+    end
+
+    it 'creates a combined current, daily, hourly weather PORO' do
+      combined = Weather.new(@current, @daily, @hourly)
+      expect(combined).to be_a(Weather)
+      expect(combined.current_weather).to be_a(CurrentWeather)
+      expect(combined.daily_weather).to be_a(DailyWeather)
+      expect(combined.hourly_weather).to be_a(HourlyWeather)
     end
   end
 end
