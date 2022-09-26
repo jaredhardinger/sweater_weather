56e1fb06c9b86586f96175ba99ef300494e0db17
Jared Hardinger <jaredhardinger@gmail.com>
2022-09-26 11:46:30 -0400

    update book service test

--- spec/services/book_service_spec.rb
+++ spec/services/book_service_spec.rb
@@ -12,6 +12,13 @@ RSpec.describe 'Book (OpenLibrary) API Service' do
     location = 'manistee,mi'
 		response = BookService.get_books(location)
 		expect(response).to be_a Hash
-
+    binding.pry
+    expect(response).to have_key(:docs)
+    expect(response[:docs].count).to eq(5)
+    book1 = response[:docs][0]
+    expect(book1).to have_key(:title)
+    expect(book1).to have_key(:publisher)
+    # expect(book1).to have_key(:isbn) there was only 1 result out of 43 that contains an isbn
+    # how to test for a 'maybe'?
 	end 
 end
