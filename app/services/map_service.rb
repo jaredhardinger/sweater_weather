ab744d1f689aca307ce8d32521b5fc81c35eaec0
Jared Hardinger <jaredhardinger@gmail.com>
2022-09-26 03:06:55 -0400

    create map service and spec

---
+++ app/services/map_service.rb
@@ -0,0 +1,18 @@
+class MapService
+  def self.get_coordinates(query)
+    response = conn.get('/geocoding/v1/address') do |req|
+      req.params['location'] = query
+      req.params['key'] = ENV['mapquest_api_key']
+    end
+    parse_json(response)
+  end
+
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+  def self.conn
+    Faraday.new(url: 'http://www.mapquestapi.com/geocoding/v1/address')
+  end
+end
---
+++ spec/services/map_service_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Mapquest API Service" do
+	before do
+		json_response = File.read('spec/fixtures/manistee_geocode.json')
+		stub_request(:get, "http://www.mapquestapi.com/geocoding/v1/address?location=manistee,mi&key=#{ENV['mapquest_api_key']}").
+				to_return(status: 200, body: json_response)
+	end
+	
+	it 'gets a response with the correct attributes' do
+		query = 'manistee,mi' 
+		response = MapService.get_coordinates(query)
+		expect(response).to be_a Hash
+    expect(response).to have_key(:results)
+    results = response[:results][0]
+    expect(results).to have_key(:locations)
+    locations = results[:locations][0]
+    expect(locations).to have_key(:latLng)
+    latLng = locations[:latLng]
+    expect(latLng).to have_key(:lat)
+    expect(latLng).to have_key(:lng)
+	end 
+end
